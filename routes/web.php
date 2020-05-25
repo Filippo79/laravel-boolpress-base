b8b1059d30e2565d958486063772b8b678db105f
Filippo Pittau <filippo.pittau@gmail.com>
2020-05-25 18:14:56 +0200

    Login User

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/', function () {
+    return view('welcome');
+});
 // Route::get('/', 'PostController@index')->name('posts.index');
 // Route::get('/published', 'PostController@indexPublished')->name('posts.published');
 Route::resource('posts', 'PostController');
+
 Route::resource('photos', 'PhotoController');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')
+->namespace('Admin')
+->middleware('auth')
+->name('admin.')
+->group(function(){
+    Route::resource('users', 'UserController');
+
+});
